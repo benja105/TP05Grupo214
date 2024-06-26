18a45d17721b98e901db1749a7df19c0e7bedd65
Benja <Benja@DESKTOP-QM1PNE7>
2024-06-25 23:50:54 -0300

    Carrera Luciano Cadena

---
+++ tp05Grupo214/src/main/java/ar/edu/unju/fi/map/CarreraMapDTO.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.map;
+
+import java.util.List;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import ar.edu.unju.fi.DTO.CarreraDTO;
+import ar.edu.unju.fi.model.Carrera;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface CarreraMapDTO {
+    @Mapping(source = "codigo", target = "codigo")
+    @Mapping(source = "nombre", target = "nombre")
+    @Mapping(source = "descripcion", target = "descripcion")
+    
+    CarreraDTO convertirCarreraACarreraDTO(Carrera c);
+
+    @InheritInverseConfiguration
+    Carrera convertirCarreraDTOACarrera(CarreraDTO cdto);
+
+    List<CarreraDTO> convertirListaCarrerasAListaCarrerasDTO(List<Carrera> listaC);
+
+    List<Carrera> convertirListaCarrerasDTOAListaCarreras(List<CarreraDTO> listaCDTO);
+}
