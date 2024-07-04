cd18711aa3b11189496f7e1be355609b663f395b
Benja <Benja@DESKTOP-QM1PNE7>
2024-07-04 00:04:51 -0300

    Carrera implementacion validaciones Cadena Luciano

--- tp05Grupo214/src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ tp05Grupo214/src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -1,8 +1,14 @@
 package ar.edu.unju.fi.model;
 
 import org.springframework.stereotype.Component;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.List;
 import lombok.Data;
 
 @Data
@@ -10,8 +16,22 @@ import lombok.Data;
 @Entity
 public class Carrera {
     @Id
+    @NotBlank(message = "Debe ingresar un código")
+    @Size(min = 3, max = 10, message = "El código debe contener como mínimo 3 caracteres y 10 como máximo")
     private String codigo;
+
+    @NotBlank(message = "Debe ingresar el nombre de la carrera")
+    @Size(min = 5, max = 50, message = "El nombre debe contener como mínimo 5 caracteres y 50 como máximo")
     private String nombre;
+
     private int cantidadAnios;
+
+    @NotBlank(message = "Debe ingresar el estado de la carrera")
     private String estado;
+
+    @OneToMany(mappedBy = "carrera", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Alumno> alumnos;
+
+    @OneToMany(mappedBy = "carrera", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Materia> materias;
 }
